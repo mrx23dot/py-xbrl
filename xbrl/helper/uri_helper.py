43c134b2c5abf8d7365504e2bba83b5b7e6fe7c7
mrx23dot <mrx23dot@gm>
2021-10-28 10:57:56 +0100

    reverted back to Manual's main

--- xbrl/helper/uri_helper.py
+++ xbrl/helper/uri_helper.py
@@ -4,9 +4,6 @@ Module containing functions for creating and resolving uri's
 import os
 import re
 
-# precompiled for speed https://stackoverflow.com/questions/1276764/stripping-everything-but-alphanumeric-chars-from-a-string-in-python
-pattern = re.compile('[\W]+')
-uri_cache = dict() # slowly growing in RAM, prevents regexp recalculations
 
 def resolve_uri(dir_uri: str, relative_uri: str) -> str:
     """
@@ -69,22 +66,10 @@ def compare_uri(uri1: str, uri2: str) -> bool:
     :param uri2:
     :return:
     """
-    # high frequency function!
+    # first remove any protocol
+    if '://' in uri1: uri1 = uri1.split('://')[1]
+    if '://' in uri2: uri2 = uri2.split('://')[1]
 
-    # uri1
-    if uri1 in uri_cache:
-        uri1new = uri_cache[uri1]
-    else:
-        # first remove any protocol
-        uri1new = pattern.sub('', uri1.split('://')[-1])   
-        uri_cache[uri1] = uri1new
-
-    # uri2
-    if uri2 in uri_cache:
-        uri2new = uri_cache[uri2]
-    else:
-        uri2new = pattern.sub('', uri2.split('://')[-1])
-        uri_cache[uri2] = uri2new
-
-    # most of the cases false
-    return uri1new == uri2new
+    uri1_segments: [str] = re.findall(r"[\w']+", uri1)
+    uri2_segments: [str] = re.findall(r"[\w']+", uri2)
+    return uri1_segments == uri2_segments
